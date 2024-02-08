cf0f36bfaa2885e7e1676214be67b850a291c4bb
anilkumarbaral <anilbaral06@gmail.ocm>
2024-02-08 15:07:26 +0530

    "sum method not required more so sum method reveted"
    This reverts commit a98472d1224ac89e13f4bf0e6380be027197fa44.

--- db.java
+++ db.java
@@ -2,12 +2,5 @@ class db
 {
   public static void main(String[]args){
    System.out.println("Hello world");
-     sum();
   }
-public void static sum(){
-	int a=10;
-    int b=20;
-   int c=a+b;
-System.out.println(c);
-}
 }
