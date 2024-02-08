2a284adff5083deb1ab48095407a9b85c349c61c
anilkumarbaral <anilbaral06@gmail.ocm>
2024-02-08 15:15:34 +0530

    Revert ""sum method  required so reverted"
    This reverts commit cf0f36bfaa2885e7e1676214be67b850a291c4bb.

--- db.java
+++ db.java
@@ -2,5 +2,12 @@ class db
 {
   public static void main(String[]args){
    System.out.println("Hello world");
+     sum();
   }
+public void static sum(){
+	int a=10;
+    int b=20;
+   int c=a+b;
+System.out.println(c);
+}
 }
